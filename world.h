78fa6e10d6796b4550600bdc9d9923599eedbe75
Scydo <79284336+Scydo@users.noreply.github.com>
2021-02-24 00:01:25 +0100

    Mini update
    Přidáno print, codename a par weather typů

--- world.h
+++ world.h
@@ -3,21 +3,27 @@
 constexpr unsigned short int HOURS = 0;
 constexpr unsigned short int MINUTES = 1;
 constexpr unsigned short int DAYS = 2;
+
 const enum class WEATHERS : const short {
 	WEATHER_CLEAR,
 	WEATHER_RAIN,
 	WEATHER_STORM,
 	WEATHER_SNOWING,
+	WEATHER_CLOUDY,
+	WEATHER_AFTERNOON,
+	WEATHER_DUSK,
 	WEATHER_COLOR
 };
 
 struct World {
 	Ethons name;
+	Ethons ingame_name;
 	float x[2], y[2];
 	WEATHERS weather;
 	uConciliate time[3];
 	World () {
-		this->name = Display(TEXTS::UNDEF_WORLD);
+		this->name = (Display(TEXTS::UNDEF_WORLD) + std::to_string(std::rand() + 0xFF));
+		this->ingame_name = Display(TEXTS::UNDEF_WORLD); 
 		this->weather = WEATHERS::WEATHER_CLEAR;
 		this->x[0] = this->y[0] = this->x[1] = this->y[1] = 100.0F;
 		time[DAYS] = time[HOURS] = time[MINUTES] = 0;
@@ -25,6 +31,7 @@ struct World {
 };
 
 namespace Worlds {
+	Function SetCodeName(World& w, std::string new_name);
 	Function SetName(World& w, std::string new_name);
 	Function SetSize(World& w, float new_x, float new_y, float new_x2, float new_y2);
 	Function UpdateTime(World& w);
@@ -35,4 +42,5 @@ namespace Worlds {
 	Function GetWeather(World& w, WEATHERS& to_wet);
 	Function SetTime(World& w, uConciliate hours, uConciliate minutes);
 	Function SetWeather(World& w, WEATHERS weather);
+	Function Print(World& w);
 }
